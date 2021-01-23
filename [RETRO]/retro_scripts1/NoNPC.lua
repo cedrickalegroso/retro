8a1e8c3b1b57da9edfb9ad067df95b388351532e
Dancedrick <personal.dancedrick@gmail.com>
2021-01-23 18:00:49 +0800

    added no cover insdie interior

--- [RETRO]/retro_scripts1/NoNPC.lua
+++ [RETRO]/retro_scripts1/NoNPC.lua
@@ -36,6 +36,25 @@ Citizen.CreateThread(function()
         end
 	end
 end)
+
+
+CreateThread(function()
+    --LAZY LOADING
+    local GetRoomKeyFromEntity = GetRoomKeyFromEntity
+    local PlayerPedId = PlayerPedId
+    local SetPlayerCanUseCover = SetPlayerCanUseCover
+    local PlayerId = PlayerId
+
+    while true do
+        if GetRoomKeyFromEntity(PlayerPedId()) ~= 0 then
+            SetPlayerCanUseCover(PlayerId(), false) -- Disable cover when ped are inside an interior
+        else
+            SetPlayerCanUseCover(PlayerId(), true) -- Enable cover when ped are outside an interior.
+        end
+        Wait(2000) -- 2 seconds is a fine delay for preserve user/pc performance.
+    end
+end)
+
 --[[
 
 
