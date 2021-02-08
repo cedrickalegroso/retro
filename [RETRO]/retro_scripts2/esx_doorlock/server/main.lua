d0709c3dd408b715b915c87b0d1d335a60877ed4
Dancedrick <personal.dancedrick@gmail.com>
2021-02-09 02:32:13 +0800

    doorlockfix

--- [RETRO]/retro_scripts2/esx_doorlock/server/main.lua
+++ [RETRO]/retro_scripts2/esx_doorlock/server/main.lua
@@ -7,7 +7,7 @@ RegisterServerEvent('esx_doorlock:updateState')
 AddEventHandler('esx_doorlock:updateState', function(doorIndex, state)
 	local xPlayer = ESX.GetPlayerFromId(source)
 
-	if xPlayer and type(doorIndex) == 'number' and type(state) == 'boolean' and Config.DoorListLOCK[doorIndex] and isAuthorized(xPlayer.job.name, Config.DoorListLOCK[doorIndex]) then
+	if xPlayer and type(doorIndex) == 'number' and type(state) == 'boolean' and Config.DoorListLOCK[doorIndex] and isAuthorized(xPlayer.job.name, xPlayer.job2.name, Config.DoorListLOCK[doorIndex]) then
 		doorState[doorIndex] = state
 		TriggerClientEvent('esx_doorlock:setDoorState', -1, doorIndex, state)
 	end
@@ -17,9 +17,9 @@ ESX.RegisterServerCallback('esx_doorlock:getDoorState', function(source, cb)
 	cb(doorState)
 end)
 
-function isAuthorized(jobName, doorObject)
+function isAuthorized(jobName, job2Name, doorObject)
 	for k,job in pairs(doorObject.authorizedJobs) do
-		if job == jobName then
+		if job == jobName or job == job2Name then
 			return true
 		end
 	end
