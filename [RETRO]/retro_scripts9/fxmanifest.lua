9619a0a7e61ef456f4da19797479a0ee3689a63a
Cedrick Alegroso <personal.dancedrick@gmail.com>
2021-02-15 21:04:25 +0800

    removed deprecated reply system

--- [RETRO]/retro_scripts9/fxmanifest.lua
+++ [RETRO]/retro_scripts9/fxmanifest.lua
@@ -30,7 +30,7 @@ server_scripts {
 
   --'sv_des.lua',
 
-  'sv_reply.lua',
+ -- 'sv_reply.lua',
 
   'sv_whitelist.lua',
 
@@ -64,7 +64,7 @@ client_scripts {
 
   --'Log/client/main.lua',
 
- 'cl_reply.lua',
+ --'cl_reply.lua',
 
   --'cl_time.lua',
   
