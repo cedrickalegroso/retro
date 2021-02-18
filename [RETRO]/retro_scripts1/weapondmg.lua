9f9c0aed874fb2627f39f03edcd7e992d01e8471
Cedrick Alegroso <personal.dancedrick@gmail.com>
2021-02-18 19:17:09 +0800

    bunot fix

--- [RETRO]/retro_scripts1/weapondmg.lua
+++ [RETRO]/retro_scripts1/weapondmg.lua
@@ -143,29 +143,16 @@ end)
 							if IsPedInAnyVehicle(playerPed, false) then
 								vehicle = GetVehiclePedIsIn(playerPed, false)
 							else
-								vehicle = GetClosestVehicle(coords, 1.5, 0, 71)
+								vehicle = GetClosestVehicle(coords, 8.0, 0, 71)
 								
 							end
 
 							if DoesEntityExist(vehicle) then
-
-								ESX.TriggerServerCallback('esx_vehiclelock:requestPlayerCars', function(isOwnedVehicle)
-									if isOwnedVehicle then
-										SetVehicleDoorOpen(vehicle, 5, false, false)
-										Citizen.Wait(2000)
-										SetVehicleDoorShut(vehicle, 5, false)
-									else 
-										SetCurrentPedWeapon(ped, GetHashKey('WEAPON_UNARMED'), true)
-                                        ESX.ShowNotification('~r~ You must be near your own car!')
-										
-										continue = false
-									end
-								end, ESX.Math.Trim(GetVehicleNumberPlateText(vehicle)))
-								
+								SetVehicleDoorOpen(vehicle, 5, false, false)
+								Citizen.Wait(2000)
+								SetVehicleDoorShut(vehicle, 5, false)
 							else
-								SetCurrentPedWeapon(ped, GetHashKey('WEAPON_UNARMED'), true)
-								ESX.ShowNotification("~r~Action Impossible~s~ : You must be near your own car!")
-								
+								ESX.ShowNotification("~r~Action Impossible~s~ : You must have a bag or be near a car trunk.")
 								continue = false
 							end
 						end
