19aa116033a626ae5619f10fff897306af3e2b48
Cedrick Alegroso <personal.dancedrick@gmail.com>
2021-02-18 18:29:31 +0800

    sal fix

--- [Base]/[essential]/es_extended/config.lua
+++ [Base]/[essential]/es_extended/config.lua
@@ -9,7 +9,7 @@ Config.ShowDotAbovePlayer   = false
 Config.DisableWantedLevel   = true
 Config.EnableHud            = false -- enable the default hud? Display current job and accounts (black, bank & cash)
 
-Config.PaycheckInterval     = 2 * 60000
+Config.PaycheckInterval     = 14 * 60000
 Config.MaxPlayers           = GetConvarInt('sv_maxclients', 255) -- set this value to 255 if you're running OneSync
 
 Config.EnableDebug          = false
